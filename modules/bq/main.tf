57ec26a0d124902888ea46b71c75b50e28c65369
akshayshekhar7 <87011412+akshayshekhar7@users.noreply.github.com>
2021-07-06 15:26:57 +0530

    Create main.tf

---
+++ modules/bq/main.tf
@@ -0,0 +1,11 @@
+resource "google_bigquery_dataset" "dataset"{
+#name    = var.dataset_name
+#for_each= toset(var.dataset_id)
+#count   = 2
+project = var.project_id
+dataset_id = var.dataset_id
+description = "This is the dataset"
+location = var.dataset_location
+
+
+}
