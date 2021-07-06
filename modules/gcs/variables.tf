809f7b6fa651cf70d34b7e762058c5840f940a0b
akshayshekhar7 <87011412+akshayshekhar7@users.noreply.github.com>
2021-07-06 15:25:52 +0530

    Create variables.tf

---
+++ modules/gcs/variables.tf
@@ -0,0 +1,35 @@
+variable "project_id" {
+  
+
+}
+
+variable "location" {
+  #default = "europe-west2"
+}
+
+variable "cmek_gcskey" {
+  default = ""
+}
+
+variable "name" {
+
+}
+
+variable "environment" {
+  default = ""
+}
+
+
+
+variable "storage_class" {
+  type    = string
+  default = "REGIONAL"
+}
+
+variable "iam_members" {
+  type = list(object({
+    role   = string
+    member = string
+  }))
+  default = []
+}
